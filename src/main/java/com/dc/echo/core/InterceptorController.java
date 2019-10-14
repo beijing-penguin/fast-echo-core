98f5d0ee5e2eb9f2f9345866c4144b725307be4b
duanchao <duanchao@spap.com>
2019-10-14 11:43:36 +0800

    更新

--- src/main/java/com/dc/echo/core/InterceptorController.java
+++ src/main/java/com/dc/echo/core/InterceptorController.java
@@ -1,9 +1,7 @@
 package com.dc.echo.core;
 
-import com.dc.echo.config.AppKeyConfig;
 import com.dc.echo.config.EchoCode;
 import com.dc.echo.pojo.Message;
-import com.dc.echo.utils.EchoCoreUtils;
 
 import io.netty.channel.ChannelHandlerContext;
 
@@ -12,12 +10,13 @@ public class InterceptorController {
 		if(msg.getHeaderObj().getMsgType()==EchoCode.HEARTBEAT_ACTION) {
 			return true;
 		}
-		if(AppKeyConfig.appKeySet.contains(msg.getHeaderObj().getAppKey())) {
-			return true;
-		}else {
-			ctx.channel().writeAndFlush(EchoCoreUtils.getMessByCode(EchoCode.UNREG_APPKEY));
-			ctx.channel().close();
-			return false;
-		}
+//		if(AppKeyConfig.appKeySet.contains(msg.getHeaderObj().getAppKey())) {
+//			return true;
+//		}else {
+//			ctx.channel().writeAndFlush(EchoCoreUtils.getMessByCode(EchoCode.UNREG_APPKEY));
+//			ctx.channel().close();
+//			return false;
+//		}
+		return true;
 	}
 }
